0de4ea1199f5ccd89cca970c2b6a6c1080e72907
Deepak kant <deepakkant07@gmail.com>
2024-03-04 18:07:08 +0530

    fix import file

--- src/DwvComponent.js
+++ src/DwvComponent.js
@@ -83,6 +83,12 @@ export const TransitionUp = React.forwardRef((props, ref) => (
   <Slide direction="up" {...props} ref={ref} />
 ))
 
+function importAll(r) {
+  let files = [];
+  r.keys().forEach((item, index) => { files.push(r(item)); });
+  return files;
+}
+
 class DwvComponent extends React.Component {
 
   constructor(props) {
@@ -431,21 +437,12 @@ class DwvComponent extends React.Component {
       app.loadURLs(this.state.dicomObj[id].image)
     }
 
-    // try {
-    //   const dirPath = '/Users/bigstep/Desktop/wish/dicom_viewer/dwv-react/src/assests/';
-    //   const folder = 'series-00000';
-    //   const filesystem = preval`
-    //   const path = require('path');
-    //   const fs = require('fs');
-    //   const files = fs.readdirSync(\`${dirPath}${folder}\`);
-    //   const filePaths = files.map(file => path.join(\`${dirPath}${folder}\`, file));
-    //   module.exports = filePaths
-    // `;
-    //  //app.loadFromUri(['http://localhost:3000/src/assests/sample/brain/1.dcm']);
-    //   console.log(filesystem,__dirname);
-    // } catch (error) {
-    //   console.log(error);
-    // }
+    try {
+      const dicImages = importAll(require.context('./assests/series-00000', false, /\.(dcm|jpe?g|svg)$/));
+      app.loadURLs(dicImages);
+    } catch (error) {
+      console.log(error);
+    }
 
   }
 
